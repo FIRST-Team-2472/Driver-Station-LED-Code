ae199d08766cc23ad3fc5608550dd777d8e5557a
Hoplite30000 <97264604+Hoplite30000@users.noreply.github.com>
2026-03-19 18:22:29 -0500

    Fixed broken patterns
    Also replaced delays with timers

--- driverStationLEDCode/driverStationLEDCode.ino
+++ driverStationLEDCode/driverStationLEDCode.ino
@@ -14,15 +14,23 @@ int mode = 0;
 int offset = 0;
 int j = 0;
 
+// Timing stuff
+int timePassedMS;
+int modeEndtime;
+int buttonEndtime;
+
 //colors
-uint32_t gold = strip.Color(212, 175, 55),
-         reddish = strip.Color(255, 0, 0),
+uint32_t gold = strip.Color(212, 80, 0),
          red = strip.Color(255, 0, 0),
          orange = strip.Color(255, 125, 0),
          yellow = strip.Color(255, 255, 0),
          green = strip.Color(0, 255, 0),
          blue = strip.Color(0, 0, 255),
          purple = strip.Color(125, 0, 255),
+         chartreuse = strip.Color(127, 255, 0),
+         skobeloff = strip.Color(0, 116, 116),
+         periwinkle = strip.Color(204, 204, 255),
+         ballKnowledge = strip.Color(135, 49, 182),
          currentColor = red;
 
 int breathingMultiplier = 1;
@@ -45,13 +53,13 @@ void setup() {
 
 void loop() {  // innefficient and could probably be optimized or made in a much better way
   // Change light mode based on button press
-  if (!digitalRead(input)) {
+  if (!digitalRead(input) && timePassedMS >= buttonEndtime) {
     mode++;
     firstTime = true;
-    if (mode >= 7) {
+    if (mode >= 10) {
       mode = 0;
     }
-    delay(170);
+    buttonEndtime = timePassedMS + 300;
   }
   // Set the lights to the correct color/pattern
   switch (mode) {
@@ -59,6 +67,7 @@ void loop() {  // innefficient and could probably be optimized or made in a much
       if (firstTime) {
         Serial.println("Starting Gold and Red Breathing");
         currentColor = red;
+        strip.setBrightness(0);
       }
       strip.fill(currentColor, 0, LED_COUNT);
       strip.setBrightness(strip.getBrightness() + (1 * breathingMultiplier));
@@ -79,14 +88,21 @@ void loop() {  // innefficient and could probably be optimized or made in a much
     case 1:  // Scrolling Gold and Red
       if (firstTime) {
         Serial.println("Starting scrolling Gold and Red");
+        strip.setBrightness(80);
         currentColor = red;
+        offset = 0;
+        modeEndtime = timePassedMS + 70;
       }
 
-      for (int i = 0; i < LED_COUNT / 2; i += 2) {
-        strip.fill(currentColor, i, i + 1);
-        strip.fill();
-        currentColor = currentColor == red ? currentColor = gold : currentColor = red;
+      if (timePassedMS >= modeEndtime) {
+        for (int i = 0; i < LED_COUNT; i += 3) {
+          strip.fill(currentColor, (i + offset) % LED_COUNT, 3);
+          currentColor = currentColor == red ? currentColor = gold : currentColor = red;
+        }
+        modeEndtime = timePassedMS + 70;
       }
+
+      offset++;
 
       break;
     case 2:  // all gold
@@ -142,169 +158,76 @@ void loop() {  // innefficient and could probably be optimized or made in a much
       if (firstTime) {
         strip.setBrightness(80);
         Serial.println("Starting scrolling Rainbow");
-        currentColor = red;
-        state = 0;
         offset = 0;
-        j = 0;
-      } else {
-        offset++;  // offset the red pixel to move the rainbow
+        modeEndtime = timePassedMS + 10;
       }
 
-      r = 255;
-      g = 0;
-      b = 0;
-
-      for (int i = 0; i < LED_COUNT; i++) {
-        if (i + offset >= 46) {
-          offset = -46;  // go back to the first pixel if we are on the last one
-        }
-        currentColor = strip.Color(r, g, b);
-        strip.setPixelColor(i + offset, currentColor);
-        switch (state) {
-          case 0:  // Red to yellow (increase green)
-            g += 34;
-            if (g >= 255) {
-              state = 1;
-              g = 255;
-            }
-            break;
-          case 1:  // Yellow to green (decrease red)
-            r -= 34;
-            if (r <= 0) {
-              state = 2;
-              r = 0;
-            }
-            break;
-          case 2:  // Green to cyan (increase blue)
-            b += 34;
-            if (b >= 255) {
-              state = 3;
-              b = 255;
-            }
-
-            break;
-          case 3:  // Cyan to blue (decrease green)
-            g -= 34;
-            if (g <= 0) {
-              state = 4;
-              g = 0;
-            }
-            break;
-          case 4:  // Blue to purple (increase red)
-            r += 34;
-            if (r >= 255) {
-              state = 5;
-              r = 255;
-            }
-
-            break;
-          case 5:  // Purple to red (decrease blue)
-            b -= 34;
-            if (b <= 0) {
-              state = 0;
-              b = 0;
-            }
-            break;
-        }
-      }
+      offset += 100;
+      strip.rainbow(offset);
 
       break;
     case 6:  // static rainbow
-      if (firstTime) {
-        currentColor = red;
-        strip.setBrightness(80);
-      }
-      for (int i = 0; i < LED_COUNT; i++) {
-        strip.setPixelColor(i, currentColor);
-        switch (state) {
-          case 0:  // Red to yellow (increase green)
-            g += 34;
-            if (g >= 255) {
-              state = 1;
-              g = 255;
-            }
-            break;
-          case 1:  // Yellow to green (decrease red)
-            r -= 34;
-            if (r <= 0) {
-              state = 2;
-              r = 0;
-            }
-            break;
-          case 2:  // Green to cyan (increase blue)
-            b += 34;
-            if (b >= 255) {
-              state = 3;
-              b = 255;
-            }
-
-            break;
-          case 3:  // Cyan to blue (decrease green)
-            g -= 34;
-            if (g <= 0) {
-              state = 4;
-              g = 0;
-            }
-            break;
-          case 4:  // Blue to purple (increase red)
-            r += 34;
-            if (r >= 255) {
-              state = 5;
-              r = 255;
-            }
-
-            break;
-          case 5:  // Purple to red (decrease blue)
-            b -= 34;
-            if (b <= 0) {
-              state = 0;
-              b = 0;
-            }
-            break;
-        }
-      }
+      strip.setBrightness(80);
+      strip.rainbow();
       break;
     case 7:  // Alternate gold and red
       strip.setBrightness(80);
       if (firstTime) {
         currentColor = red;
+        modeEndtime = timePassedMS + 500;
       }
+
       strip.fill(currentColor, 0, LED_COUNT);
-
-      if (currentColor == red) currentColor = gold;
-      else currentColor = red;
-      delay(750);
+      if (timePassedMS >= modeEndtime) {
+        if (currentColor == red) currentColor = gold;
+        else currentColor = red;
+        modeEndtime = timePassedMS + 500;
+      }
       break;
-
-    case 8:  // EXTRA: 20 Random pixels with random color (red or gold)
-      // Get random number generator
-      std::random_device rd;
-      std::mt19937 gen(rd());
-      std::uniform_int_distribution<> distr(0, 45);
-
-      // get a random pixel then set a random color
-      for (int i = 0; i < 20; i++) {
-        int pixelNumber = distr(gen);
-
-        std::uniform_int_distribution<> colorDist(1, 2);
-
-        strip.setPixelColor(pixelNumber, colorDist(gen) == 1 ? red : gold);
+    case 8:  // GRADLE DEMON COLORS (BALL KNOWLEDGE)
+      if (firstTime) {
+        strip.setBrightness(80);
+        currentColor = skobeloff;
+        offset = 0;
+        modeEndtime = timePassedMS + 70;
       }
 
-      strip.setBrightness(80);
-
-      delay(100);
-
-      for (int i = 500; i > 0; i++) {
-        strip.setBrightness(strip.getBrightness() - 0.16);
-        delay(1);
+      if (firstTime) {
+        Serial.println("Starting scrolling Gold and Red");
+        strip.setBrightness(80);
+        currentColor = skobeloff;
+        offset = 0;
+        modeEndtime = timePassedMS + 200;
       }
 
+      if (timePassedMS >= modeEndtime) {
+        for (int i = 0; i < LED_COUNT; i += 3) {
+          strip.fill(currentColor, (i + offset) % LED_COUNT, 3);
+          if (currentColor == skobeloff) {
+            currentColor = chartreuse;
+          } else if (currentColor == chartreuse) {
+            currentColor = periwinkle;
+          } else if (currentColor == periwinkle) {
+            currentColor = ballKnowledge;
+          } else if (currentColor == ballKnowledge) {
+            currentColor = skobeloff;
+          }
+        }
+        modeEndtime = timePassedMS + 200;
+      }
+
+      offset++;
+
+    break;
+    case 9:  // off
+      strip.setBrightness(0);
+      strip.clear();
       break;
   }
 
   firstTime = false;
 
+  timePassedMS += 10;
 
   strip.show();
   Serial.println(mode);
